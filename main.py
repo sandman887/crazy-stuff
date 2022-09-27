dcdff693ec15d155ae10672a65d40188a8e68354
sandman887 <114511152+sandman887@users.noreply.github.com>
2022-09-27 22:44:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+ 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+
+url = 'https://www.sfit.ac.in/'
+reqs = requests.get(url,headers=headers)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+ 
+urls = []
+
+while(True):
+    for link in soup.find_all('a'):
+        # print(link.get('href'))
+        requests.get(url,headers=headers)
